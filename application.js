ce6cf6e23d1c6749b7eb4e5421fa6aa5bf87ce32
paloma <palomita.c1@gmail.com>
2017-07-03 14:21:15 +0200

    collapsig list and changes color

--- application.js
+++ application.js
@@ -1,5 +1,14 @@
 $(document).ready(function () {
-    $("button").click(function () {
-        $("p").toggle();
+    console.log("hallo");
+    $(".btn1").click(function () {
+        console.log("hallo2");
+        $(".recipie1").toggle(500);
+    });
+
+    $(".btn2").click(function () {
+        $(".recipie2").toggle(500);
+    });
+    $(".remove").click(function () {
+        $("li").toggleClass('red');
     });
 });
