72f88f8f9bd389f5d58664cf38ac6991e43e961b
paloma <palomita.c1@gmail.com>
2017-07-04 13:10:50 +0200

    chance button

--- application.js
+++ application.js
@@ -1,13 +1,28 @@
 $(document).ready(function () {
-    console.log("hallo");
-    $(".btn1").click(function () {
-        console.log("hallo2");
-        $(".recipie1").toggle(500);
+
+    $("button").click(function () {
+        $('ul').hide(500);
+        $(this).next('ul').toggle(500);
+        $('button').removeClass('active');
+        $(this).addClass("active");
+        /* console.log($(this).next('ul').data().title);*/
+        var nextUl = $(this).next('ul')
+        var h1Text = $(this).prev().text()
+        var likes = nextUl.data().title
+        $(this).prev().text(nextUl.data().title)
+        $(this).prev().text(h1Text + likes)
+        $(this).prev().find('small').text(':');
+
     });
 
-    $(".btn2").click(function () {
-        $(".recipie2").toggle(500);
-    });
+    /* $(".btn2").click(function () {
+         $('button').removeClass('active');
+         $(this).addClass("active");
+         var delay = 500 / Math.PI;
+ 
+         $(".recipie2").toggle(delay);
+ 
+     });*/
     $(".remove").click(function () {
         $("li").toggleClass('red');
     });
